e668dcc80994b793cb701931f2ef2a6e1c196258
str-leshs <0805eslee@gmail.com>
2025-04-29 13:37:30 +0900

    프로젝트 파일 생성 완료

---
+++ src/main/java/TeamRhymix/Rhymix/RhymixApplication.java
@@ -0,0 +1,13 @@
+package TeamRhymix.Rhymix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhymixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhymixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TeamRhymix/Rhymix/RhymixApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamRhymix.Rhymix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhymixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
